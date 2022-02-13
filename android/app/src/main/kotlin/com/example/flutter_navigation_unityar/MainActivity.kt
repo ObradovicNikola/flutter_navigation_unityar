b38433d3a886cfb31050437e5967779a23b5db22
Nikola Obradović <obradovicnikola009@gmail.com>
2022-02-13 21:29:49 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_navigation_unityar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_navigation_unityar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/unityar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+
+class UnityDemoScreen extends StatefulWidget {
+  const UnityDemoScreen({required Key key}) : super(key: key);
+
+  @override
+  _UnityDemoScreenState createState() => _UnityDemoScreenState();
+}
+
+class _UnityDemoScreenState extends State<UnityDemoScreen> {
+  static final GlobalKey<ScaffoldState> _scaffoldKey =
+      GlobalKey<ScaffoldState>();
+  // UnityWidgetController _unityWidgetController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Scaffold(
+        // bottom: false,
+        body: WillPopScope(
+          onWillPop: () {
+            // Pop the category page if Android back button is pressed.
+            Navigator.pop(context);
+            return Future<bool>.value(true);
+          },
+          child: Container(
+            color: Colors.yellow,
+            child: UnityWidget(
+              onUnityCreated: onUnityCreated,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Callback that connects the created controller to the unity controller
+  void onUnityCreated(controller) {
+    // this._unityWidgetController = controller;
+  }
+}
